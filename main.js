2d11eefc696d2708df31215ce9be2418ed20980c
Jacey Davis <jaceyd923@gmail.com>
2018-12-01 18:32:25 -0700

    added images for starships

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import { starships } from './assets/starships.js'
 
-console.log(starships)
+let shipsWithCredits = starships.filter(starship => starship.cost_in_credits !== "unknown")
+
+// LINKS EACH STARSHIP WITH ITS IMAGE --------------------
+shipsWithCredits.forEach(starship => {
+    if (starship.url[32] === "/") {
+        starship.id = starship.url[31]
+    } else {
+        starship.id = `${starship.url[31]}${starship.url[32]}`
+    }
+    starship.img = `images/${starship.id}.jpg`
+})
