c358414e54e7d5b37ffb9763c22a7ef5f04605e5
Jacey Davis <jaceyd923@gmail.com>
2018-12-01 19:34:49 -0700

    added header and createListing ability

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 import { starships } from './assets/starships.js'
 
 let shipsWithCredits = starships.filter(starship => starship.cost_in_credits !== "unknown")
+let starshipListingsContainer = document.querySelector('#starship-listings-container')
+let credits = 0;
 
-// LINKS EACH STARSHIP WITH ITS IMAGE --------------------
+// Gives each starship an id (used for adding images) --------------------
 shipsWithCredits.forEach(starship => {
     if (starship.url[32] === "/") {
         starship.id = starship.url[31]
     } else {
         starship.id = `${starship.url[31]}${starship.url[32]}`
     }
-    starship.img = `images/${starship.id}.jpg`
 })
+
+let createListing = (starship) => {
+    let fig = document.createElement('figure')
+    fig.classList.add('starship-listing')
+    starshipListingsContainer.appendChild(fig)
+
+    let img = document.createElement('img')
+    img.src = `images/${starship.id}.jpg`
+    let cap = document.createElement('figcaption')
+    cap.textContent = starship.name
+    fig.appendChild(img)
+    fig.appendChild(cap)
+}
+
+// shipsWithCredits.forEach(starship => {createListing(starship)})
